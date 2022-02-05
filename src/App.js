8fe0b48e2dcf00fea631e51f8b8b95151abd82c1
Matthew Lese <matthewjlese@gmail.com>
2022-02-05 11:19:09 -0800

    uses state to set number of forms to display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import TodoForm from './components/todo_form';
+import React, { useState } from 'react'
 
 function App() {
+  const [forms, setForms] = useState([<TodoForm key={0} />])
+
+  const addForm = () => {
+    setForms([...forms, <TodoForm key={forms.length} />])
+  }
+  
   return (
     <div className="App">
-      <TodoForm />
+      {forms}
+      <button onClick={e => addForm()}>Add another form</button>
     </div>
   );
 }
