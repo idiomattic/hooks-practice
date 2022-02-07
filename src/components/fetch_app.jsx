bcca64bad7b5e07cfebceebc8668e31a93bcbcc6
Matthew Lese <matthewjlese@gmail.com>
2022-02-07 13:07:50 -0800

    fetches from other API on mount

--- src/components/fetch_app.jsx
+++ src/components/fetch_app.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const FetchApp = props => {
   const [fact, setFact] = useState('')
+  const [fruitData, setFruitData] = useState([])
+
+  useEffect(async () => {
+    const response = await fetch('https://whiskyhunter.net/api/distilleries_info/', {
+      method: 'GET',
+      mode: 'cors'
+    })
+    const data = await response.json()
+    console.log(data.slice(0, 10))
+    setFruitData(data.slice(0, 10))
+  }, [])
   
-  const getRandomFact = async () => {
+  const getCatFact = async () => {
     const response = await fetch('https://catfact.ninja/fact')
     const data = await response.json()
     setFact(data.fact)
@@ -11,8 +22,16 @@ const FetchApp = props => {
 
   return(
     <div>
-      <button onClick={e => getRandomFact()}>Get a Cat Fact</button>
+      <button onClick={e => getCatFact()}>Get a Cat Fact</button>
       <h3>{fact}</h3>
+      <br />
+      <ul>
+        {fruitData.map((datum, i) => (
+          <li key={i}>
+            <p>{datum.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 
